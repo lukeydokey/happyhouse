8d024bd28c37fd0361cf048d217c1aef1a4e4504
박찬호 <taurus42977@gmail.com1~>
2022-05-19 12:52:00 +0900

    구조화

--- Front/happyhousevue/src/components/board/BoardList2.vue
+++ Front/happyhousevue/src/components/board/BoardList2.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-import http from "@/api/http";
+import { listArticle } from "@/api/board";
 
 export default {
   name: "BoardList",
@@ -45,9 +45,21 @@ export default {
     };
   },
   created() {
-    http.get(`/board`).then(({ data }) => {
-      this.articles = data;
-    });
+    let param = {
+      pg: 1,
+      spp: 20,
+      key: null,
+      word: null,
+    };
+    listArticle(
+      param,
+      (response) => {
+        this.articles = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   },
   methods: {
     moveWrite() {
