cf3b012f0f9c6162dc424f524c0eb66689eaf7eb
박찬호 <taurus42977@gmail.com1~>
2022-05-22 14:10:22 +0900

    마크 마우스오버 이벤트 추가

--- Front/happyhousevue/src/components/house/HouseMap.vue
+++ Front/happyhousevue/src/components/house/HouseMap.vue
@@ -114,6 +114,14 @@ export default {
             this.houses.data[step].lat,
             this.houses.data[step].lng,
           ),
+          content:
+            this.houses.data[step].sidoName +
+            " " +
+            this.houses.data[step].gugunName +
+            " " +
+            this.houses.data[step].dongName +
+            " " +
+            this.houses.data[step].jibun,
         });
       }
       this.displayMarkers(this.markerPositions);
@@ -222,12 +230,42 @@ export default {
       const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
 
       positions.forEach((position) => {
+        console.log(position);
         const marker = new kakao.maps.Marker({
           map: this.map,
           position: position.latlng,
           title: position.title,
           image: markerImage,
         });
+
+        var infowindow = new kakao.maps.InfoWindow({
+          content:
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<h5 id="firstHeading" class="firstHeading"> ' +
+            position.title +
+            " </h5>" +
+            '<div id="bodyContent"><p>' +
+            position.content +
+            "</div>", // 인포윈도우에 표시할 내용
+        });
+
+        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+        // 이벤트 리스너로는 클로저를 만들어 등록합니다
+        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+        kakao.maps.event.addListener(
+          marker,
+          "mouseover",
+          this.makeOverListener(this.map, marker, infowindow),
+          // overlay.setMap(this.map);
+        );
+        kakao.maps.event.addListener(
+          marker,
+          "mouseout",
+          this.makeOutListener(this.map, marker, infowindow),
+        );
+
         this.markers.push(marker);
       });
 
@@ -241,6 +279,24 @@ export default {
       if (this.map.getLevel() > 10) {
         this.moveMap(this.houses.data[0]);
       }
+    },
+    makeOverListener(map, marker, infowindow) {
+      return function () {
+        const imgSrc = require("@/assets/map/apart.png");
+        const imgSize = new kakao.maps.Size(48, 70);
+        const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
+        marker.setImage(markerImage);
+        infowindow.open(map, marker);
+      };
+    },
+    makeOutListener(map, marker, infowindow) {
+      return function () {
+        const imgSrc = require("@/assets/map/apart.png");
+        const imgSize = new kakao.maps.Size(42, 63);
+        const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
+        marker.setImage(markerImage);
+        infowindow.close();
+      };
     },
   },
   mounted() {
@@ -303,5 +359,97 @@ export default {
   padding: 0px;
   width: 100vw;
   height: 100px;
+}
+.wrap {
+  position: absolute;
+  left: 0;
+  bottom: 40px;
+  width: 288px;
+  height: 132px;
+  margin-left: -144px;
+  text-align: left;
+  overflow: hidden;
+  font-size: 12px;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  line-height: 1.5;
+}
+.wrap * {
+  padding: 0;
+  margin: 0;
+}
+.wrap .info {
+  width: 286px;
+  height: 120px;
+  border-radius: 5px;
+  border-bottom: 2px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+  background: #fff;
+}
+.wrap .info:nth-child(1) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.info .title {
+  padding: 5px 0 0 10px;
+  height: 30px;
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-size: 18px;
+  font-weight: bold;
+}
+.info .close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #888;
+  width: 17px;
+  height: 17px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png");
+}
+.info .close:hover {
+  cursor: pointer;
+}
+.info .body {
+  position: relative;
+  overflow: hidden;
+}
+.info .desc {
+  position: relative;
+  margin: 13px 0 0 90px;
+  height: 75px;
+}
+.desc .ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.desc .jibun {
+  font-size: 11px;
+  color: #888;
+  margin-top: -2px;
+}
+.info .img {
+  position: absolute;
+  top: 6px;
+  left: 5px;
+  width: 73px;
+  height: 71px;
+  border: 1px solid #ddd;
+  color: #888;
+  overflow: hidden;
+}
+.info:after {
+  content: "";
+  position: absolute;
+  margin-left: -12px;
+  left: 50%;
+  bottom: 0;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.info .link {
+  color: #5085bb;
 }
 </style>
