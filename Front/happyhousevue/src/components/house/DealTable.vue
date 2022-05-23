7154ffe8adc40ed3755901f83abcb117963ab510
soo1095 <pudingles@gmail.com>
2022-05-23 14:52:40 +0900

    거래 매물 페이지 추가

--- Front/happyhousevue/src/components/house/DealTable.vue
+++ Front/happyhousevue/src/components/house/DealTable.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <h5><b>거래 매물</b></h5>
-    <b-table striped hover :items="this.deals" :fields="fields"></b-table>
+    <b-table
+      striped
+      hover
+      :items="this.deals"
+      :current-page="curPage"
+      :per-page="perPage"
+      :fields="fields"
+    ></b-table>
+    <b-pagination
+      align="center"
+      v-model="curPage"
+      :per-page="perPage"
+      :total-rows="this.deals.length"
+    ></b-pagination>
   </div>
 </template>
 
@@ -12,6 +25,8 @@ const houseStore = "houseStore";
 export default {
   data() {
     return {
+      curPage: 1,
+      perPage: 10,
       // Note 'isActive' is left out and will not appear in the rendered table
       fields: [
         {
